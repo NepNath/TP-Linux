0ff806528c3003cdff65aefaef88486db2baf03e
NepNath <neptonath@gmail.com>
2024-03-04 12:30:04 +0100

    Update fonnybash.sh

--- fonnybash.sh
+++ fonnybash.sh
@@ -8,6 +8,7 @@
     echo "OS : $(cat /etc/os-release | head -n 1 | cut -d '"' -f 2) and kernel version is $(uname -r)"
     echo "IP : $(ip a | head -n 9 | tail -n 1 | cut -c 1-20)" #pas correcte faut récupérer l'ip et pas la cut dans ip a
     echo "Top 5 processes by RAM usage :"
+    ps aux --sort=-%mem | head -5 | cut -c 1-66 
     echo "Disk : $(df / -h | tail -1 | cut -c 33-39) space left"
 
 
